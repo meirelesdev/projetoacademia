ca609249caca2121d377c7c1a52003d242e6adab
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-06-03 06:41:20 -0300

    Iniciando repositorio

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Index() {
+    return(
+        <>
+        <h1>Bem Vindo ao projeto Hcodelab Fase 6</h1>
+        <h2>Tema do projeto: Academia</h2>
+        </>
+    )
+}
