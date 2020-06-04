61af2fdf5aa7f17039f62a326a778bae974b2917
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-06-04 20:36:47 -0300

    Pasta componentes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
+import Sectionblog from '../components/Sectionblog'
 
 export default function Index() {
     return(
         <>
-        <h1>Bem Vindo ao projeto Hcodelab Fase 6</h1>
-        <h2>Tema do projeto: Academia</h2>
+          <h1>Bem Vindo ao projeto Hcodelab Fase 6</h1>
+          <h2>Tema do projeto: Academia</h2>
+          <Sectionblog />
+             
         </>
     )
 }
