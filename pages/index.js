12cac49b91f392e17f2f726613633877ec91092a
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-06-04 21:56:40 -0300

    Css global

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
+import App from 'next/app'
+
 import Sectionblog from '../components/Sectionblog'
 
 export default function Index() {
     return(
         <>
-          <h1>Bem Vindo ao projeto Hcodelab Fase 6</h1>
-          <h2>Tema do projeto: Academia</h2>
-          <Sectionblog />
-             
+            <Head />
+            <h1>Bem Vindo ao projeto Hcodelab Fase 6</h1>
+            <h2>Tema do projeto: Academia</h2>
+            <Sectionblog />
         </>
     )
 }
