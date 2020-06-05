1cca1ace327c8cd7bde23bdddbe05e9460f9cab3
filipesndev <filipe.eduardo.neiva@gmail.com>
2020-06-04 22:00:19 -0300

    ey

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import styles from './Header.module.css'
+
+export default () => {
+    return (
+        <>
+            <div className={styles["headerfundo"]}>
+                
+                <img className={styles["logo"]} src="/Logo/Fitness.svg" />
+                
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+            </div>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Index() {
     return(
         <>
-        <h1>Bem Vindo ao projeto Hcodelab Fase 6</h1>
-        <h2>Tema do projeto: Academia</h2>
+            <Header/>
+            
         </>
     )
 }
