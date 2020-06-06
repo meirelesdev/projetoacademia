2095e9ec7581b5d1f9ee75e7ea7f9d97f13e9915
filipesndev <filipe.eduardo.neiva@gmail.com>
2020-06-06 18:02:41 -0300

    footer

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import App from 'next/app'
 import Sectionblog from '../components/Sectionblog'
+import Link from 'next/link'
+import Footer from '../components/Footer'
 
 
 export default function Index() {
@@ -11,6 +13,7 @@ export default function Index() {
             <Head />
                 
             <Sectionblog />
+            <Footer/>
         </>
     )
 }
