3ede87d4404633ab2e3fafb561b113cf749c2a1d
Igor Mondoni <igor.hm17@hotmail.com>
2020-06-08 16:58:42 -0300

    continuing studentArea

--- pages/studentArea.js
+++ pages/studentArea.js
@@ -2,6 +2,10 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import styles from '../components/Studentarea.module.css'
 import Head from 'next/head'
+import React, {useState} from 'react'
+import Link from 'next/link'
+import axios from 'axios'
+import { Cookies } from 'react-cookie'
 
 
 export default function Studentarea() {
@@ -13,15 +17,15 @@ export default function Studentarea() {
                 <div className='formularioaluno'></div>
                 <form>
                     <div className={styles.container}>
-                        <div>
-                            <div>
+                        <div className={styles.container_input}>
+                            <div className={styles.mid_input}>
                                 <label htmlFor="email">E-mail</label>
-                                <input type="email" name="email" id="email" className={styles.mid_input} />
+                                <input type="email" name="email" id="email" className={styles.input_student}/>
 
                             </div>
-                            <div>
+                            <div className={styles.mid_input}>
                                 <label htmlFor="password">Senha</label>
-                                <input type="password" name="password" id="password" className={styles.mid_input} />
+                                <input type="password" name="password" id="password" className={styles.input_student}/>
 
                             </div>
 
